1c9b96ce0b33d9439bb65a8cfbf98cf095b64dad
jerry <jerry@xx.com>
2020-05-06 15:30:32 +0400

    rest api mux

---
+++ router.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: HomePage")
+
+}
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	//fmt.Fprintf(w, "Key: "+key)
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	//fmt.Fprintf(w, "%+v", string(reqBody))
+
+	var article Article
+
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, article := range Articles {
+
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+			reqBody, _ := ioutil.ReadAll(r.Body)
+
+			var article Article
+			//_ = json.NewEncoder(r.Body).Decode(&article)
+			json.Unmarshal(reqBody, &article)
+			article.Id = vars["id"]
+			Articles = append(Articles, article)
+			json.NewEncoder(w).Encode(&article)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("update article")
+	//w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var updateArticle Article
+
+	for index, article := range Articles {
+
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+
+			reqBody, _ := ioutil.ReadAll(r.Body)
+			json.Unmarshal(reqBody, &updateArticle)
+			fmt.Println(updateArticle)
+			//file, _ := os.open(r.Body)
+			//json.Unmarshal(reqBody, &updateArticle)
+			//json.NewEncoder(r.Body).Decode(&updateArticle)
+			Articles = append(Articles, updateArticle)
+			json.NewEncoder(w).Encode(updateArticle)
+			return
+		}
+	}
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+
+	log.Fatal(http.ListenAndServe(":8787", myRouter))
+}
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
